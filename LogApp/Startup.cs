169711766d67f7575062a031c444947ac0863022
Eryx <eryxjose@hotmail.com>
2020-10-13 12:09:53 -0300

    Primeiro commit

---
+++ LogApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LogApp.Startup))]
+namespace LogApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
