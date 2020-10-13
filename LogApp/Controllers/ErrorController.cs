bde9b1051af769ab97961c0e900a45ae3efc155f
Eryx <eryxjose@hotmail.com>
2020-10-13 12:21:03 -0300

    Configurações log4net e redirecionamento páginas erro customizadas.

---
+++ LogApp/Controllers/ErrorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LogApp.Controllers
+{
+    public class ErrorController : Controller
+    {
+
+        public ActionResult Index(Exception error)
+        {
+            Response.StatusCode = 500;
+            return View("Error", error);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+        public ViewResult NotFound(Exception error)
+        {
+            Response.StatusCode = 404;  //you may want to set this to 200
+            return View("NotFound", error);
+        }
+
+        public ViewResult NotAuthorized(Exception error)
+        {
+            Response.StatusCode = 401;  //you may want to set this to 200
+            return View("NotAuthorized", error);
+        }
+    }
+}
--- LogApp/Global.asax.cs
+++ LogApp/Global.asax.cs
@@ -1,3 +1,4 @@
+using log4net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,58 @@ namespace LogApp
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        
+        private static readonly ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+            var ex = Server.GetLastError().GetBaseException();
+
+            log.Error("Erro.", ex);
+
+            Server.ClearError();
+
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "Error");
+
+            if (ex.GetType() == typeof(HttpException))
+            {
+                var httpException = (HttpException)ex;
+                var code = httpException.GetHttpCode();
+
+                switch (code)
+                {
+                    case 401:
+                        routeData.Values.Add("action", "NotAuthorized");
+                        break;
+                    case 404:
+                        routeData.Values.Add("action", "NotFound");
+                        break;
+                    default:
+                        routeData.Values.Add("action", "Index?auth=s");
+                        break;
+                }
+            }
+            else
+            {
+                routeData.Values.Add("action", "Index?auth=s");
+            }
+
+            routeData.Values.Add("error", ex);
+
+            IController errorController = new webportos.Controllers.ErrorController();
+            errorController.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
         }
     }
 }
